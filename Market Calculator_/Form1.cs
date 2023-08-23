a247048dd576d9e15bf1a6f6bedc25314ea0e0d9
Dubys007 <136149598+Dubys007@users.noreply.github.com>
2023-08-23 17:16:50 +0100

    Add files via upload

---
+++ Market Calculator_/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_TASK
+{
+    public partial class Form1 : Form
+    {
+
+        Double value = 0;
+        String operation = "";
+        bool operation_pressed = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Calculator calculator = new Calculator();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+
+            operation_pressed = false;
+            Button b = (Button)sender;  //converted to button 
+            result.Text = result.Text + b.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            if ((result.Text == "can't divide by zero"))
+            {
+                result.Text = "0";
+
+
+            }
+            Button b = (Button)sender;
+
+            if (value != 0)
+            {
+                button14.PerformClick();
+                operation_pressed = true;
+                operation = b.Text;
+                equation.Text = value + "" + operation;
+            }
+            else
+            {
+
+            
+
+            operation = b.Text;
+            value = double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + "" + operation;
+            }
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            operation_pressed = false;
+
+            equation.Text = "";
+            switch (operation)
+            {
+                case "+":
+                    
+                    result.Text = Calculator.Add(value, double.Parse(result.Text)).ToString();
+                    break;
+
+                case "-":
+                   
+                    result.Text = Calculator.Subtract(value, double.Parse(result.Text)).ToString();
+                    break;
+
+                case "*":
+                   
+                    result.Text = Calculator.Multiply(value, double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                   
+                    try
+                    {
+                        result.Text = Calculator.Divide(value, double.Parse(result.Text)).ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        result.Text = ex.Message.ToString();
+
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+            value = int.Parse(result.Text);
+            operation = "";
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+    }
+    }
+
---
+++ Test_Calculator/Calculator_unit_test.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_TASK;
+using Xunit;
+
+namespace Test_Calculator
+{
+    public class Calculator_unit_test
+    {
+        [Fact]
+
+        public void AdditionTest()
+        {
+            // Arrange
+
+            double expected = 37;
+
+            // Act
+
+            double actual = Calculator.Add(25, 12);
+
+            // Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+
+        public void SubtractionTest()
+
+        {
+            // Arrange
+
+            double expected = 10;
+
+            // Act
+
+            double actual = Calculator.Subtract(20, 10);
+
+            // Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+
+        public void MultiplicationTest()
+
+        {
+            // Arrange
+
+            double expected = 20;
+
+            // Act
+
+            double actual = Calculator.Multiply(10, 2);
+
+            // Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+
+        public void DivisionTest()
+
+        {
+            // Arrange
+
+            double expected = 25;
+
+            // Act
+
+            double actual = Calculator.Divide(50, 2);
+
+            // Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+   
+
+    }
+}
